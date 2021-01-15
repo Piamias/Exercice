2de79363db9cc1125a8b40eda960c051dfed07bb
Victor Piamias <victorpiamiaspro@outlook.fr>
2021-01-15 20:44:45 +0100

    New

--- id.c
+++ id.c
@@ -3,20 +3,20 @@
 #include "struct.h"
 
 int main()
-{
-	Perso player[2];
+{		
+	Perso player[5];
 	int i;
 
 	i = 0;
-	while (i < 2)
+	while (i < 5)
 	{
 		id(player, i);
 		i++;
 	}
 	i = 0;
-	while (i < 2)
+	while (i < 5)
 	{	
-	printf("\nPlayer %d = \n%s\n%s\n%s\n%d\n%s\n",(i+1), player[i].fname, player[i].lname, player[i].nat, player[i].age, player[i].sex);
+	printf("\nPlayer %d = \nFirst name = %s\nLast name = %s\nNationality = %s\nAge = %d\nSexe = %s\n",(i+1), player[i].fname, player[i].lname, player[i].nat, player[i].age, player[i].sex);
 	i++;
 	}
 }
